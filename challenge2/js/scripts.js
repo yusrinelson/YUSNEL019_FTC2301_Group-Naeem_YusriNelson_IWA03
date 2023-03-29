474f36afa25c8c1cd837eb9f1658453916fd4ab1
yusrinelson <125978974+yusrinelson@users.noreply.github.com>
2023-03-29 14:45:11 +0200

    created and edited html, js code

---
+++ challenge2/js/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+import {role as nwabisa} from './nwabisa.js'
+import {role as johannes} from './johannes.js'
+import {role as alex} from './alex.js'
+
+console.log('Roles:', nwabisa, ',', johannes , ',', alex);
